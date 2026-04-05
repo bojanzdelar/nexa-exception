b6f8ec98f76b901e5f46fb20cc8af047d22c1367
Bojan Zdelar <bojan.zdelar@gmail.com>
2026-04-05 11:32:09 +0200

    Added missing exception

--- src/main/java/com/zdelar/nexa/exception/handler/SecurityExceptionHandler.java
+++ src/main/java/com/zdelar/nexa/exception/handler/SecurityExceptionHandler.java
@@ -1,30 +1,29 @@
 package com.zdelar.nexa.exception.handler;
 
 import com.zdelar.nexa.exception.api.ApiError;
+import java.time.Instant;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.Instant;
 
 @ConditionalOnClass(name = "org.springframework.security.core.AuthenticationException")
 @RestControllerAdvice
 public class SecurityExceptionHandler {
 
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ApiError> handleAuth(AuthenticationException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ApiError("Unauthorized", 401, Instant.now(), null));
-    }
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<ApiError> handleAuth(AuthenticationException ex) {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(new ApiError("Unauthorized", 401, Instant.now(), null));
+  }
 
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ApiError> handleForbidden(Exception ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ApiError("Access denied", 403, Instant.now(), null));
-    }
-
+  @ExceptionHandler({AccessDeniedException.class, AuthorizationDeniedException.class})
+  public ResponseEntity<ApiError> handleForbidden(Exception ex) {
+    return ResponseEntity.status(HttpStatus.FORBIDDEN)
+        .body(new ApiError("Access denied", 403, Instant.now(), null));
+  }
 }
